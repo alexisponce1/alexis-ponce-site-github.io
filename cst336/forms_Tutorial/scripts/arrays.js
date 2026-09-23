f9dd2f8ffe4f7e7e44a586aa8c52b6ca7900d7ad
alexisponce1 <aleponce@csumb.edu>
2026-09-22 22:01:04 -0700

    lab2 completed

--- cst336/forms_Tutorial/scripts/arrays.js
+++ cst336/forms_Tutorial/scripts/arrays.js
@@ -22,3 +22,66 @@ for (let color of colors){
     console.log(color);
 }
 
+
+let ages = [50, 40, 20, 25, 80];
+let average = 0;
+
+for (let age of ages){
+    average += age;
+}
+
+average = average/ages.length;
+console.log(average);
+
+
+
+let checkboxs = document.querySelectorAll("input[name=toppings]");
+let totalChecked = 0;
+
+for (let checkbox of checkboxs){
+    if (checkboxs.checked){
+        totalChecked++;
+    }
+}
+
+let newParagraph = document.createElement("p");
+
+
+newParagraph.textContent = "I have been created";
+newParagraph.id = "dynamic-paragraph"
+
+let insertArea = document.querySelector('#insert-area');
+insertArea.appendChild(newParagraph);
+
+
+let names = ['mario', 'luma', 'rosalina', 'yoshi'];
+
+for (let name of names){
+    let paragraph = document.createElement("p");
+    paragraph.textContent = name;
+    insertArea.appendChild(paragraph);
+}
+
+let nameDropdown = document.querySelector("#names-dropdown");
+for (let name of names){
+    let nameItem = document.createElement("option");
+    nameItem.textContent = name;
+    nameItem.value = name;
+    nameDropdown.appendChild(nameItem);
+}
+
+let limit = 20;
+
+
+//"async" is like a stamp that lets the OS knowthat this function could take
+// an unknown amount of time to finish
+async function getCats(){
+    let catsResult = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+    console.log(catsResult);
+
+    let catsData = await catsResult.json();
+    console.log(catsData);
+}
+
+getCats();
+
--- cst336/lab2/scripts/script1.js
+++ cst336/lab2/scripts/script1.js
@@ -1,5 +1,6 @@
 
-let answer = 28;
+let answer = Math.floor(Math.random() * 99) + 1;
+console.log(answer);
 
 let guesses = 0;
 
@@ -10,40 +11,81 @@ let guessInput = document.querySelector("#guessInput");
 let guessMessage = document.querySelector("#guessMessage");
 let numGuesses = document.querySelector("#numGuesses");
 let guessCount = document.querySelector("#guessCount");
+let restButton = document.querySelector("#resetButton");
+let gamesWon = document.querySelector("#gamesWon");
+let gamesLost = document.querySelector("#gamesLost");
+let lossCount = 0;
+let winCount = 0;
+
+restButton.style.display = "none"
 
 const winMessage = "Congratulations you guessed it";
-const lowMessage = "To low guess again";
-const highMessage = "To high guess again";
-const outOfBounds = "Please Enter new number"
+const lowMessage = "Too low guess again";
+const highMessage = "Too high guess again";
+const outOfBounds = "Number is out of bounds"
 const toManyGuesses = "Ran out of guesses"
 
 
+
+restButton.addEventListener("click", function () {
+    answer = Math.floor(Math.random() * 99) + 1;
+
+    guesses = 0;
+
+    guessInput.value = "";
+
+    guessMessage.textContent = "";
+
+    numGuesses.textContent = "";
+
+    guessCount.textContent = "Attempts: 0";
+
+    restButton.style.display = "none"
+    guessButton.style.display = "inline"
+
+})
+
+
 guessButton.addEventListener("click", function () {
-    if (guesses >= 7) {
-        guessMessage.textContent = toManyGuesses;
-    }
-    else if (+guessInput.value > 99 || +guessInput.value < 1) {
+    if (+guessInput.value > 99 || +guessInput.value < 1) {
         guessMessage.textContent = outOfBounds
         guessMessage.style.color = "red";
 
     } else if (+guessInput.value === answer) {
         guessMessage.textContent = winMessage;
         guessMessage.style.color = "green";
+
+        winCount++;
+        gamesWon.textContent = "Games Won: " + winCount;
+
         numGuesses.append(guessInput.value + " ")
         guesses += 1
-        guessCount.append("Attempts: " + guesses)
-        
-    } else if (+guessInput.value <= answer) {
+        guessCount.textContent = "Attempts: " + guesses
+        guessButton.style.display = "none"
+        restButton.style.display = "inline"
+
+    } else if (+guessInput.value < answer) {
         guessMessage.textContent = lowMessage;
         guessMessage.style.color = "red";
         numGuesses.append(guessInput.value + ", ")
         guesses += 1
-        
-    } else if (+guessInput.value >= answer) {
+        guessCount.textContent = "Attempts: " + guesses
+
+    } else if (+guessInput.value > answer) {
         guessMessage.textContent = highMessage;
         guessMessage.style.color = "red";
         numGuesses.append(guessInput.value + ", ")
         guesses += 1
-        
+        guessCount.textContent = "Attempts: " + guesses
+    }
+
+    if (guesses >= 7 && +guessInput.value !== answer) {
+        guessMessage.textContent = toManyGuesses + ", The number was " + answer;
+        guessMessage.style.color = "red";
+
+        lossCount++;
+        gamesLost.textContent = "Games Lost: " + lossCount;
+        guessButton.style.display = "none"
+        restButton.style.display = "inline"
     }
 });
